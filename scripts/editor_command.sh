ca1602d1f24d008ab19581817048a8f08c070f9b
Eric Liang <eric@bmxlabs.com>
2024-06-20 01:48:04 +0800

    don't change path if not in sub dir

--- scripts/editor_command.sh
+++ scripts/editor_command.sh
@@ -2,4 +2,4 @@
 sed 's/.*(\(.*\).*)/mv "\1" "." /' articles | bash
 
 # 2. remove index path for selected files
-cat SUMMARY.md.bak | awk -F'/' '{print substr($1, 1, length($1)-2)$3}' > SUMMARY.md
+cat SUMMARY.md.bak2 | awk -F'/' '{if($3 != "") print substr($1, 1, length($1)-2)$3; else print $0}' > SUMMARY.md
